9b28352859a36c01de850754c6ddf55b6369d40e
Miguel Palhas <mpalhas@gmail.com>
2012-09-06 00:35:57 +0100

    added matches initial actions

--- db/migrate/20120828132208_create_play_futsal_matches.rb
+++ db/migrate/20120828132208_create_play_futsal_matches.rb
@@ -1,8 +1,8 @@
 class CreatePlayFutsalMatches < ActiveRecord::Migration
   def up
     create_table :play_futsal_matches do |t|
-      t.integer :home_team_id, :null => false
-      t.integer :away_team_id, :null => false
+      t.integer :home_team_id
+      t.integer :away_team_id
 
       t.integer :home_score, :null => false, :default => 0
       t.integer :away_score, :null => false, :default => 0
