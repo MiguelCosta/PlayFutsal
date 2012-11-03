2522d046f84785191b823005e4e23c2f8d37743c
Miguel Palhas <mpalhas@gmail.com>
2012-11-03 16:39:57 +0000

    updated phases migration

--- db/migrate/20121016231228_create_play_futsal_phases.rb
+++ db/migrate/20121016231228_create_play_futsal_phases.rb
@@ -2,7 +2,7 @@ class CreatePlayFutsalPhases < ActiveRecord::Migration
   def change
     create_table :play_futsal_phases do |t|
       t.string :name
-      t.integer :order
+      t.integer :position
 
       t.timestamps
     end
